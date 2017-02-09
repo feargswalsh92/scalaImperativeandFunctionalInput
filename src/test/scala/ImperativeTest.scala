51ff1db5bd6fee5b6491ba8d9d059b9ce040ba79
Feargal Walsh <feargswalsh@gmail.com>
2017-02-08 20:04:22 -0600

    Beginning to implement functional, pushing so that I have a working copy to come back to in case I mess this up as I am adding two more main classes

--- src/test/scala/ImperativeTest.scala
+++ src/test/scala/ImperativeTest.scala
@@ -2,13 +2,12 @@ package scala.com
 import org.scalatest.{BeforeAndAfterEach, FunSuite }
 import scala.io.Source._
 
-class imperativeTest extends FunSuite with BeforeAndAfterEach {
 
+  class ImperativeHistogramTest extends FunSuite with BeforeAndAfterEach {
   test("testInputList") {
    val inputData = fromFile("inputData/input.txt").getLines
    val expectedList = List("3", "4", "5", "7", "8", "10", "45")
    assert((new imperativeProgram).constructList(inputData).sorted == expectedList.sorted)
-
 
     }
 
@@ -22,6 +21,10 @@ class imperativeTest extends FunSuite with BeforeAndAfterEach {
       //tst to check if array has been constructed properly
 
     }
+
+
+
+
 
 
   //list = new
